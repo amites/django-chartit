1b96f4c8e755e8a279131e30fb1cb9a54de6b765
Julian Amaya <julian@monoku.com>
2012-03-14 09:36:17 -0500

    it now works on mootools

--- chartit/static/chartit/js/chartloader.js
+++ chartit/static/chartit/js/chartloader.js
@@ -1,7 +1,8 @@
 // jQuery function to create a chart for each of the HighCharts Chart Options
 // JSON object (_chartit_hco_array) passed to web page from the view. 
-$(document).ready(function() {
-	$.each(_chartit_hco_array, function(index, chartoptions) {
+// window.addEvent('domready', function(){});
+window.addEvent('domready', function() {
+	_chartit_hco_array.each(function(chartoptions, index) {
 		chart = new Highcharts.Chart(chartoptions);
 	});
 });
